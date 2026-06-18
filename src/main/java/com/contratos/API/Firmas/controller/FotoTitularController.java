66a84ffe90c5968b3d259b910f4cbf5128cb2230
Arturo Duarte <arturoortigoza2001@gmail.com>
2026-06-18 11:55:05 -0600

    listo

--- src/main/java/com/contratos/API/Firmas/controller/FotoTitularController.java
+++ src/main/java/com/contratos/API/Firmas/controller/FotoTitularController.java
@@ -10,12 +10,13 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/titulares")
+
 @RestController
 @RequestMapping("/api/titulares")
 public class FotoTitularController {
